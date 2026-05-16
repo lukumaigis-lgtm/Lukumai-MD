3a9f6f948c975a85299b4f640fadba1920efa88a
lukumaigis-lgtm <samwellukumai860@gmail.com>
2026-05-16 16:26:47 +0300

    Create Lukumai.js

---
+++ Lukumai.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Lukumai Website</title>
+</head>
+<body>
+    <h1>Karibu kwenye Website yangu</h1>
+    <p>Hii ni project yangu ya GitHub.</p>
+</body>
+</html>
