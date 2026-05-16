adf819175cda8b2f2a61895a72e07c7150bc1f84
lukumaigis-lgtm <samwellukumai860@gmail.com>
2026-05-16 16:24:24 +0300

    Create Samwel.js

---
+++ Samwel.js
@@ -0,0 +1,12 @@
+# Lukumai-MD
+
+Hii ni project yangu ya kwanza GitHub.
+
+## Maelezo
+Project hii inafanya:
+- Kuhifadhi files
+- Kushare code
+- Kujifunza programming
+
+## Mwandishi
+Samwel Lukumai
